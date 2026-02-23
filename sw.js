4cb03996e297331f5c9cbf5720722aca38d76b0a
Sadman Sakib <90724482+SADMAN30102001SAKIB@users.noreply.github.com>
2026-02-23 21:02:44 +0600

    Add additional files to service worker cache

--- sw.js
+++ sw.js
@@ -1,19 +1,30 @@
 const CACHE = "qr-scanner-v1";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE).then(cache =>
       cache.addAll([
         "./",
         "./index.html",
-        "./manifest.json"
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png",
+        "./favicon-32.png",
+        "./favicon-16.png"
       ])
     )
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(
+      cached => cached || fetch(event.request)
+    )
   );
 });
