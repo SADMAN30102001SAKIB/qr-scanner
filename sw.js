54acf26d619fd4373ae9c71ba35e8f92206400e8
Sadman Sakib <90724482+SADMAN30102001SAKIB@users.noreply.github.com>
2026-02-23 20:45:38 +0600

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "qr-scanner-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
